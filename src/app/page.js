4dd9d6c7bbf9b0b043bf709a2efd82ac3b709117
RinaBhalani <rina.teamflourish@gmail.com>
2024-08-02 12:40:38 +0530

    solve merge error

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,21 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Homesection from "@/component/Homesection";
 import Testimoniam from "@/component/Testimoniam";
+import Client from '@/sections/Client'
+import ContactForm from '@/sections/ContactForm'
+import GraphicDesign from '@/sections/GraphicDesign'
+import Provide from '@/sections/Provide'
 
 export default function Home() {
   return (
     <main >
     
       <Homesection/>
+      <Provide/>
+      <Client/>
+      <GraphicDesign/>
       <Testimoniam/>
+      <ContactForm/>
     </main>
   );
 }
